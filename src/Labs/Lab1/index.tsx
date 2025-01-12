8a5e65e6751a26331294cad4a34b7a2ae96215d4
yuhsuan <buybtsticket@gmail.com>
2025-01-12 15:42:57 -0500

    update profile class

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -225,7 +225,7 @@ export default function Lab1(){
 
         <div id = "wd-textarea">
         <h5>Textarea</h5>
-            <textarea cols="50" rows="5"
+            <textarea cols={50} rows={5}
             placeholder="Biography"
             title="tooltip">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
@@ -351,4 +351,5 @@ export default function Lab1(){
         Please <a href="http://www.lipsum.com" id="wd-lipsum"> click here </a>
         to get dummy text<br/>
 
-</div>);}
+</div>
+);}

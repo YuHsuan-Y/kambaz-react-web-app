2b2cc5ca6f8b5ef80148a3a772aa5eb7d30a090c
yuhsuan <buybtsticket@gmail.com>
2025-01-12 11:54:53 -0500

    first commit

---
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -0,0 +1,164 @@
+export default function Editor(){
+    return(
+        <div id="wd-course-assignment-editor">
+            <label htmlFor="wd-name">Assignment Name</label><br/>
+            <br/>
+            <input id="wd-name" value="A1 - ENV + HTML" /> <br/><br/>
+            
+            <textarea id="wd-description" cols={45} rows={9}>
+                The assignment is available online Submit a link to the landing page of
+                your Web application running on Netlify. The landing page should include the following:
+                Your full name and section Links to each of the lab assignments Link to the Kanbas
+                application Links to all relevant source code repositories The Kanbas application 
+                should include a link to navigate back to the landing page.
+            </textarea>
+            <br />
+            <table>
+                <tr>
+                    <td align="right" valign="top">
+                        <label htmlFor="wd-points">Points</label>
+                    </td>
+                    <td>
+                        <input id="wd-points" defaultValue={100}/>
+                    </td>
+                </tr>
+                <br/>
+
+                <tr>
+                    <td align="right" valign="top">
+                        <label htmlFor="wd-group">Assignment Group</label>
+                    </td>
+                    <td>
+                        <select id="wd-group" defaultValue="ASSIGNMENTS">
+                            <option value="ASSIGNMENTS">ASSIGNMENTS</option>
+                            <option value="PROJECTS">PROJECTS</option>
+                            <option value="QUIZZES">QUIZZES</option>
+                            <option value="EXAMS">EXAMS</option>
+                            <option value="PARTICIPATION">PARTICIPATION</option>
+                        </select>
+                    </td>
+                </tr>
+                <br/>
+
+                <tr>
+                    <td>
+                    <label htmlFor="wd-display-grade-as">Display Grade as</label>
+                    </td>
+                    <td>
+                        <select defaultValue="Percentage" id="wd-display-grade-as">
+                            <option value="PERCENTAGE">Percentage</option>
+                            <option value="REALGRADE">Real Grade</option>
+                            <option value="LETTERGRADE">Letter Grade</option>
+                        </select><br/>
+                    </td>
+                </tr>
+                <br/>
+
+                <tr>
+                    <td>
+                        <label htmlFor="wd-submission-type"> Submission Type</label>
+                    </td>
+                    <td>
+                        <select id="wd-submission-type" defaultValue="Online">
+                            <option value="ONLINE">Online</option>
+                            <option value="ONPAPER">On Paper</option>
+                            <option value="EXTERNALTOOL">External Tool</option>
+                            <option value="NOSUBMISSION">No Submission</option>
+                        </select><br/>
+                    </td>
+                </tr>
+                <br/>
+
+                <tr>
+                    <td>
+                    <br/>
+                    </td>
+
+                    <td>
+                        <label htmlFor="wd-online-entry-options">Online Entry Options</label><br/>
+                        <input type= "checkbox" name="check-online-entry-options"  id="wd-text-entry"></input>
+                        <label htmlFor="wd-text-entry">Text Entry</label><br/>
+
+                        <input type="checkbox" name="check-online-entry-options" id="wd-website-url"></input>
+                        <label htmlFor="wd-website-url">Website URL</label><br/>
+
+                        <input type="checkbox" name="check-online-entry-options" id="wd-media-recordings"></input>
+                        <label htmlFor="wd-media-recordings">Media Recordings</label><br/>
+
+                        <input type="checkbox" name="check-online-entry-options" id="wd-student-annotaion"></input>
+                        <label htmlFor="wd-student-annotation">Student Annotation</label><br/>
+
+                        <input type="checkbox" name="check-online-entry-options" id="wd-file-upload"></input>
+                        <label htmlFor="wd-file-upload">File Uploads</label>
+                    </td><br/>
+                </tr>
+                <br/>
+            </table>
+            
+            <table>
+                <tr>
+                    <td>
+                    <br/>
+                    </td>
+                
+                    <td align="left" valign="top">
+                        <label htmlFor="wd-assign-to">Assign Assign to</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <br/>
+                    </td>
+                    <td align="right" valign="middle">
+                        <input id="wd-assign-to" type="text" defaultValue="Everyone"/>
+                    </td>
+                </tr><br/>
+
+                <tr>
+                    <td>
+                    <br/>
+                    </td>
+                    <td align="left" valign="middle">
+                        <label htmlFor="wd-due-date">Due</label><br/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    <br/>
+                    </td>
+                    <td>
+                        <input type="date" id="wd-due-date" value="2024-05-13"/>
+                    </td>
+                </tr><br/>
+
+                <tr>
+                    <td>
+                    <br/>
+                    </td>
+                    <td>
+                        <label htmlFor="wd-available-from">Available From</label>
+                    </td>
+                 
+                    <td>
+                        <label htmlFor="wd-available-until">Until</label>
+                    </td>
+                    
+                </tr>
+
+                <tr>
+
+                    <td>
+                        <input type="date" id="wd-available-from" value="2024-05-06"/>
+                    </td>
+
+                    <td>
+                    <br/>
+                    </td>
+                    <td>
+                        <input type="date" id="wd-available-until" value="2024-05-20"/>
+                    </td>
+                </tr>
+
+            </table>
+        </div>
+);}
---
+++ src/Kambaz/Dashboard.tsx
@@ -0,0 +1,108 @@
+import { Link } from "react-router-dom";
+export default function Dashboard() {
+  return (
+    <div id="wd-dashboard">
+      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      <div id="wd-dashboard-courses">
+        
+        <div className="wd-dashboard-course">
+          <Link to="/Kambaz/Courses/1234/Home"
+                className="wd-dashboard-course-link" >
+            <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <h5> CS1234 React JS </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer  </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+      <div>
+        <Link to="/Kambaz/Courses/1234/Home"
+            className="wd-dashboard-course-link">
+          <img src="/images/reactjs.jpg" width={200} />
+          <div>
+              <h5> CS4550 12631 Web Development </h5>
+              <p className="wd-dashboard-CS4550">
+                CS4550.125631.202410
+              </p>
+              <button> Go </button>
+          </div>
+        </Link>
+      </div>
+
+      <div>
+        <Link to="/Kambaz/Courses/1234/Home"
+          className="wd-dashboard-course-link">
+            <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <h5> CS5002 12130 Discrete Math </h5>
+              <p className="wd-dashboard-CS5002">
+                CS5002.12130.202410
+              </p>
+              <button> Go </button>
+            </div>
+        </Link>
+      </div>
+
+      <div>
+        <Link to="/Kambaz/Courses/1234/Home"
+          className="wd-dashboard-course-link">
+          <img src="/images/reactjs.jpg" width={200} />
+          <div>
+            <h5> CS5800 50294 Algorithm </h5>
+            <p className="wd-dashboard-CS5800">
+              CS5800.50294.202450
+            </p>
+            <button> Go </button>
+          </div>
+        </Link>
+      </div>
+
+      <div>
+        <Link to="/Kambaz/Courses/1234/Home"
+          className="wd-dashboard-course-link">
+            <img src="/images/reactjs.jpg" width={200}/>
+          <div>
+            <h5> CS5004 39596 Object-Oriented Design </h5>
+            <p className="wd-dashboard-CS5004">
+              CS5004.39596.202430
+            </p>
+            <button> Go </button>
+          </div>
+        </Link>
+      </div>
+      
+      <div>
+        <Link to="/Kambaz/Courses/1234/Home"
+          className="wd-dashboard-courses-link">
+            <img src="/images/reactjs.jpg" width={200}/>
+          <div>
+            <h5> CS5005 39599 Recitation for CS </h5>
+            <p className="wd-dashboard-CS5005">
+              CS5005.39599.202430
+            </p>
+            <button> Go </button>
+          </div>
+        </Link>
+      </div>
+
+      <div>
+        <Link to="/Kambaz/Courses/1234/Home"
+          className="wd-dashboard-courses-link">
+            <img src="/images/reactjs.jpg" width={200} />
+          <div>
+            <h5> CS5001 12121 Python </h5>
+            <p className="wd-dashboard-CS5001">
+              CS5001.12121.202409
+            </p>
+            <button> Go </button>
+          </div>
+        </Link>
+      </div>
+
+      </div>
+    </div>
+);}
